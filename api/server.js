2a1a33373ec228e42ddd6026b1b4946aefe8e931
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-12-15 14:21:18 -0800

    setup and register endpoint done

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+const db = require("../data/dbconfig");
+
+const router = require("./router");
+
+server.use(express.json());
+server.use(cors());
+server.use("/api", router);
+
+server.get("/", (req, res) => {
+  console.log("working");
+  res.status(200).json("You did it!!!");
+});
+
+module.exports = server;
